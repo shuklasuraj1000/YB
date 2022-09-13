4f89e9d7e84d7ddcd3cd0351fccf4fc5058134b0
SOORAJ SHUKLA <105222437+shuklasuraj1000@users.noreply.github.com>
2022-09-13 08:12:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 from flask import Flask, render_template, request
 from flask_cors import CORS,cross_origin
+from selenium import webdriver
 from selenium.webdriver import Chrome
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -41,7 +42,13 @@ def index():
             col5 = []
             col6 = []
             col8 = []
-            with Chrome(r"C:\Users\Admin\PycharmProjects\ImageScrapper\chromedriver.exe") as driver:
+	    chrome_options = webdriver.ChromeOptions()
+            chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+            chrome_options.add_argument("--headless")
+            chrome_options.add_argument("--disable-dev-shm-usage")
+            chrome_options.add_argument("--no-sandbox")
+            driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+            #with Chrome(r"C:\Users\Admin\PycharmProjects\ImageScrapper\chromedriver.exe") as driver:
                 wait = WebDriverWait(driver, 2)
                 driver.get(youtube_url)
 
@@ -97,7 +104,13 @@ def index2():
             youtube_url = searchString
             col1 = []
             col2 = []
-            with Chrome(r"C:\Users\Admin\PycharmProjects\ImageScrapper\chromedriver.exe") as driver:
+	    chrome_options = webdriver.ChromeOptions()
+            chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+            chrome_options.add_argument("--headless")
+            chrome_options.add_argument("--disable-dev-shm-usage")
+            chrome_options.add_argument("--no-sandbox")
+            driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+            #with Chrome(r"C:\Users\Admin\PycharmProjects\ImageScrapper\chromedriver.exe") as driver:
                 wait = WebDriverWait(driver, 2)
                 driver.get(youtube_url)
 
